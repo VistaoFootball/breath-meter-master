978aa224b42f4c311dc2c86caae637c95740bb59
Vistaofootball <vistao.sports@gmail.com>
2023-01-28 15:00:30 +0100

    Updates

---
+++ precache-manifest.8890c39390f25443ac5e5724bb0bf103.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b471b9420bb488008f46e3502d40ed89",
+    "url": "/breath-meter-master/index.html"
+  },
+  {
+    "revision": "a392a88a865c9bfd0192",
+    "url": "/breath-meter-master/static/css/main.2db55151.chunk.css"
+  },
+  {
+    "revision": "90c9f39b86e79bd5a1ca",
+    "url": "/breath-meter-master/static/js/2.1d14430f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/breath-meter-master/static/js/2.1d14430f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a392a88a865c9bfd0192",
+    "url": "/breath-meter-master/static/js/main.8cf52b87.chunk.js"
+  },
+  {
+    "revision": "84de9afc8c635cfd50ac",
+    "url": "/breath-meter-master/static/js/runtime-main.b8a8700e.js"
+  }
+]);
